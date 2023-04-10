0101fb33c8ebac5d88e77a459bcc9dce26c4d42c
Fonck <AFonck@protonmail.com>
2023-04-10 11:56:16 -0600

    pages basic structure.

---
+++ javascript/autotyper.js
@@ -0,0 +1,21 @@
+
+var typed = new Typed('.auto-type', {
+    strings: ['"This is the real secret of life; to be completely engaged with what you are doing in the here and now. And instead of calling it work, realize it is play." - Alan Watts',
+     '"If you want to find the secrets of the universe, think in terms of energy, frequency and vibration." - Nikola Tesla',
+      '"Knowing is not enough; we must apply. Willing is not enough; we must do." - Bruce Lee',
+      '"Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning." - Albert Einstein',
+      '"The world will not be destroyed by those who do evil, but by those who watch them without doing anything." - Albert Einstein',
+      '"The future is already here - it is just not evenly distributed." - William Gibson',
+      '"If you think you are too small to make a difference, try sleeping with a mosquito." - Dalai Lama',
+      '"Be the change you want to see in the world." - Mahatma Gandhi',
+      '"Once you understand the way broadly, you will see it in everything." - Miyamoto Musashi',
+      '"We were born to work together like feet, hands and eyes, like the two rows of teeth, upper and lower. To obstruct each other is unnatural." - Marcus Aurelius',
+      '"Never doubt that a small group of thoughtful, commited citiznes can the world; indeed, it is the only thing that ever has." - Margaret Mead',
+      '"Do not count the days, make the days count." - Muhammad Ali',
+      '"The best way to predict the future is to invent it." - Alan Kay'],
+    typeSpeed: 80,
+    backSpeed: 2,
+    shuffle: true,
+    backDelay: 3000,
+    loop: true
+  });
